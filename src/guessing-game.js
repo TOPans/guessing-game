5b84d623aaccbd8c209561e53ad0f14623d9ad8b
TOPans <TOPans@mail.ru>
2021-08-16 12:09:53 +0300

    feat: implement class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.left = min;
+      this.right = max;
     }
-
+  
     guess() {
-
+      return Math.round((this.left + this.right) / 2);
     }
-
+  
     lower() {
-
+      this.right = Math.round((this.left + this.right) / 2);
     }
-
+  
     greater() {
-
+      this.left = Math.round((this.left + this.right) / 2);
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
